99b4e8b191e501abd2af39db7525bb1c1c047dca
Sergey Soldatov <sasolda3@mts.ru>
2021-01-12 18:32:38 +0300

    log & reqID to echo context

--- providers/httpsrv/echo/middlewares.go
+++ providers/httpsrv/echo/middlewares.go
@@ -6,6 +6,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/rs/zerolog"
 )
 
 // CORSDefault allows requests from any origin wth GET, HEAD, PUT, POST or DELETE method.
@@ -37,12 +38,30 @@ func HydrationRequestID() echo.MiddlewareFunc {
 		return func(c echo.Context) error {
 			requestID := c.Request().Header.Get("x-request-id")
 			if requestID != "" {
-				return nil
+				return next(c)
 			}
 
 			id, _ := uuid.NewRandom()
 			c.Request().Header.Set("x-request-id", id.String())
-			return nil
+			return next(c)
+		}
+	}
+}
+
+const zerologWithReqID = "garageZerologWithReqID"
+
+func HydrationLogger(log *zerolog.Logger) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			requestID := c.Request().Header.Get("x-request-id")
+			if requestID != "" {
+				c.Set(zerologWithReqID, log)
+			} else {
+				l := log.With().Str("x-request-id", requestID).Logger()
+				c.Set(zerologWithReqID, &l)
+			}
+
+			return next(c)
 		}
 	}
 }
