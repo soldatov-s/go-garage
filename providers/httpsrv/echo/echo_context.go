2799309647dbbdc5bfc624ea06ddc608218e5a4a
Sergey Soldatov <sasolda3@mts.ru>
2021-01-12 18:25:11 +0300

    added log & reqID to echo context

--- providers/httpsrv/echo/echo_context.go
+++ providers/httpsrv/echo/echo_context.go
@@ -2,8 +2,17 @@ package echo
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/rs/zerolog"
 )
 
 type Context interface {
 	echo.Context
+}
+
+func GetReqID(ec Context) string {
+	return ec.Request().Header.Get("x-request-id")
+}
+
+func GetLog(ec Context) *zerolog.Logger {
+	return ec.Get(zerologWithReqID).(*zerolog.Logger)
 }
