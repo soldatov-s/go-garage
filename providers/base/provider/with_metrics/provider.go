53a9c5cbafcecb16efea501f8e8320d09c83f4b5
Sergey Soldatov <sasolda3@mts.ru>
2021-01-13 09:06:57 +0300

    fixed provider with metrics

--- providers/base/provider/with_metrics/provider.go
+++ providers/base/provider/with_metrics/provider.go
@@ -9,9 +9,9 @@ import (
 
 type Entity interface {
 	provider.Entity
-	GetMetrics(string) map[string]*stats.MetricOptions
-	GetAliveHandlers(string) map[string]stats.CheckFunc
-	GetReadyHandlers(string) map[string]stats.CheckFunc
+	GetMetrics(string) stats.MapMetricsOptions
+	GetAliveHandlers(string) stats.MapCheckFunc
+	GetReadyHandlers(string) stats.MapCheckFunc
 }
 
 // Provider provides abstract worker.
@@ -28,33 +28,33 @@ func NewProvider(ctx context.Context, providersName, providerName string) *Provi
 }
 
 // GetMetrics return map of the metrics from provider
-func (bp *Provider) GetMetrics(prefix string) (stats.MapMetricsOptions, error) {
+func (bp *Provider) GetMetrics(prefix string) stats.MapMetricsOptions {
 	metrics := make(stats.MapMetricsOptions)
 	bp.Entitys.Range(func(_, v interface{}) bool {
 		metrics.Fill(v.(Entity).GetMetrics(prefix))
 		return true
 	})
-	return metrics, nil
+	return metrics
 }
 
 // GetAliveHandlers return array of the aliveHandlers from provider
-func (bp *Provider) GetAliveHandlers(prefix string) (stats.MapCheckFunc, error) {
+func (bp *Provider) GetAliveHandlers(prefix string) stats.MapCheckFunc {
 	handlers := make(stats.MapCheckFunc)
 	bp.Entitys.Range(func(_, v interface{}) bool {
 		handlers.Fill(v.(Entity).GetAliveHandlers(prefix))
 		return true
 	})
-	return handlers, nil
+	return handlers
 }
 
 // GetReadyHandlers return array of the readyHandlers from provider
-func (bp *Provider) GetReadyHandlers(prefix string) (stats.MapCheckFunc, error) {
+func (bp *Provider) GetReadyHandlers(prefix string) stats.MapCheckFunc {
 	handlers := make(stats.MapCheckFunc)
 	bp.Entitys.Range(func(_, v interface{}) bool {
 		handlers.Fill(v.(Entity).GetReadyHandlers(prefix))
 		return true
 	})
-	return handlers, nil
+	return handlers
 }
 
 // Shutdown should shutdown all known entitys.
@@ -83,4 +83,24 @@ func (bp *Provider) Start() error {
 		return true
 	})
 	return err
+}
+
+// StartEnity starts connection things like watchers and queues.
+func (bp *Provider) StartEnity(enityName string) error {
+	v, ok := bp.Entitys.Load(enityName)
+	if ok {
+		return v.(Entity).Start()
+	}
+
+	return nil
+}
+
+// ShutdownEnity starts connection things like watchers and queues.
+func (bp *Provider) ShutdownEnity(enityName string) error {
+	v, ok := bp.Entitys.Load(enityName)
+	if ok {
+		return v.(Entity).Shutdown()
+	}
+
+	return nil
 }
