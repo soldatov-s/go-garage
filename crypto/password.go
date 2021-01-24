edf60d93a14cb309c2781c8d870ca7e641decba7
Sergey Soldatov <sasolda3@mts.ru>
2021-01-24 17:21:23 +0300

    fixed crypto

--- crypto/password.go
+++ crypto/password.go
@@ -2,13 +2,12 @@ package crypto
 
 import "golang.org/x/crypto/bcrypt"
 
-func HashAndSalt(pwd []byte) (string, error) {
-	// Use GenerateFromPassword to hash & salt pwd.
-	// MinCost is just an integer constant provided by the bcrypt
-	// package along with DefaultCost & MaxCost.
-	// The cost can be any value you want provided it isn't lower
-	// than the MinCost (4)
-	hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MinCost)
+const (
+	defaultCost = 8
+)
+
+func HashAndSalt(pwd string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(pwd), defaultCost)
 	if err != nil {
 		return "", err
 	}
@@ -18,7 +17,5 @@ func HashAndSalt(pwd []byte) (string, error) {
 }
 
 func ComparePasswords(hashedPwd, plainPwd string) error {
-	// Since we'll be getting the hashed password from the DB it
-	// will be a string so we'll need to convert it to a byte slice
 	return bcrypt.CompareHashAndPassword([]byte(hashedPwd), []byte(plainPwd))
 }
