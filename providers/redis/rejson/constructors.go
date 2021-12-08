267c4c57966972f632cf43b64be0c58f4b8182d3
Sergey Soldatov <ssacompany@mail.ru>
2021-12-08 21:44:17 +0300

    feat(providers): redis: added emtyclient

--- providers/redis/rejson/constructors.go
+++ providers/redis/rejson/constructors.go
@@ -5,6 +5,17 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
+func NewEmptyClient() *Client {
+	return &Client{}
+}
+
+func (cl *Client) SetConn(client *redis.Client) {
+	cl.Client = client
+	cl.redisProcessor = &redisProcessor{
+		Process: client.Process,
+	}
+}
+
 func ExtendClient(client *redis.Client) *Client {
 	return &Client{
 		client,
